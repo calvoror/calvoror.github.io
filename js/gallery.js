424123ec7741336a313d71d0c164beebe593f808
Roberto Calvo Rodriguez <roberto.calvorodriguez@viseo.com>
2018-08-31 17:26:59 +0200

    Gallery enhanced

--- js/gallery.js
+++ js/gallery.js
@@ -5,12 +5,12 @@ function activateGallery() {
     let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
     let mainImage = document.querySelector("#gallery-photo img");
 
-    thumbnails.forEach(function(thumbnail) {
-        thumbnail.addEventListener("click", function() {
-            // Set clicked image as main image.
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.addEventListener("click", function () {
             let newImageSrc = thumbnail.dataset.largeVersion;
-            mainImage.setAttribute("src", newImageSrc);
-            mainImage.setAttribute("alt", thumbnail.alt);
+            // Preload large images
+            let largeImage = new Image();
+            largeImage.src = newImageSrc;
 
             const CURRENT_CLASS = "current";
 
@@ -19,11 +19,20 @@ function activateGallery() {
             thumbnail.parentNode.classList.add(CURRENT_CLASS);
 
             let galleryInfo = document.querySelector("#gallery-info");
-            let title       = galleryInfo.querySelector(".title");
+            let title = galleryInfo.querySelector(".title");
             let description = galleryInfo.querySelector(".description");
 
             title.innerHTML = thumbnail.dataset.title;
             description.innerHTML = thumbnail.dataset.description;
+
+            // Set clicked image as main image.
+            mainImage.style.opacity = 0.8;
+
+            setTimeout(function () {
+                mainImage.style.opacity = 1;
+                mainImage.setAttribute("src", newImageSrc);
+                mainImage.setAttribute("alt", thumbnail.alt);
+            }, 200);
         });
     });
 }
